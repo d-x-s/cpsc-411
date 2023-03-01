bafdabf65311dc3664639d96c0dae4585af629d1
Davis Song <dsong04@students.ubc.ca>
2023-02-28 16:35:48 -0800

    Add Feb.14 lecture material

--- Lecture/Feb.14 monadic-skeleton.rkt
+++ Lecture/Feb.14 monadic-skeleton.rkt
@@ -8,9 +8,32 @@ e ::= (let ([x e]) e) | (if e e e) | (binop e e) | integer? | x
 |#
 
 ;; TODO 1: Implement an interpreter for Source
+
+;; e-> integer?
+;; evaluates the program to a value
 (define (interp-source e)
-  (void)
-  
+
+  (define EMPTY-ENV '())
+  #; (define EMPTY-ENV (make-immutable-hash))
+
+  (define (interp-binop e v1 v2)
+    (match e
+      ['+ (+ v1 v2)]
+      ['- (- v1 v2)]
+      ['* (* v1 v2)]
+    )
+  )
+
+  (define (interp-e e env)
+    (match e
+    [`(let ([,x1, e1]) ,e2) (void)]
+    [`(if ,pred ,e1 ,e2) (void)]
+    [`(,binop ,e1 ,e2) (void)]
+    [(? symbol?) (void)]
+    [(? integer) (void)]
+    ) 
+  )
+  (interp-e -e EMPTY-ENV)
 )
 
 #|
@@ -25,6 +48,21 @@ C ::= N | (let ([x C]) C) | (if V C C)
 ;; TODO 2: Implement the monadic form transformation for Source.
 (define (monadic-form e)
   (void))
+
+(module+ test
+  (require rackunit)
+  (check-equal?
+    (interp-source '(if 0 1 2))
+    2)
+
+  (check-equal?
+    (interp-source '(if 1 1 2))
+    1)
+
+  (check-equal?
+    (interp-source '())
+    2)
+  )
 
 ;; TODO 3: Implement an interpreter for Monadic, in 1 line of code.
 (define (interp-monadic e)
